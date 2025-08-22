823bc7dd6887d2c761feeb51c49ebb6e50586d19
krishna-tikariya03 <krishnatikariya.md03@gmail.com>
2025-08-22 14:23:06 +0530

    Form Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Header from './Header.js';
+import { validation } from '../utlis/validation.js';
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+
+  const handelSubmit = () =>{
+    console.log(email.current.value);
+    console.log(password.current.value);
+    const msg = validation(email.current.value, password.current.value);
+    if (msg) {
+      setErrorMessage(msg.message);
+      return;
+    }
+  }
+
   const toggleForm = () => {
     setIsSignInForm(!isSignInForm);
   };
@@ -15,11 +32,14 @@ const Login = () => {
 
       <div className='text-white absolute p-10 bg-black/80 rounded-md w-4/12 mx-auto mt-32 right-0 left-0 '>
         <h1 className='text-4xl font-bold'>Sign {isSignInForm === true ? "In" : "UP"}</h1>
-        <form className='gap-5 flex flex-col mt-5 '>
+        <form onSubmit={(e)=> e.preventDefault()} className='gap-5 flex flex-col mt-5 '>
           {!isSignInForm && (<input type='text' placeholder='Full Name' className='p-4 rounded-sm bg-white/10 text-white border border-white/50' />)}
-          <input type='email' placeholder='Email Address' className='p-4 rounded-sm bg-white/10 text-white border border-white/50' />
-          <input type='password' placeholder='Password' className=' p-4 rounded-sm bg-white/10 text-white border border-white/50' />
-          <button className='w-full p-2 my-2 bg-[rgb(229,9,20)] rounded-md text-white'>Sign {isSignInForm === true ? "In" : "UP"}</button>
+          <input ref={email} type='email' placeholder='Email Address' className='p-4 rounded-sm bg-white/10 text-white border border-white/50' />
+          <input ref={password} type='password' placeholder='Password' className=' p-4 rounded-sm bg-white/10 text-white border border-white/50' />
+
+          <p className='text-red-500 text-sm font-bold'>{errorMessage}</p>
+
+          <button onClick={handelSubmit} className='w-full p-2 my-2 bg-[rgb(229,9,20)] rounded-md text-white'>Sign {isSignInForm === true ? "In" : "UP"}</button>
         </form>
 
         <div className='mt-5 space-y-3'>
